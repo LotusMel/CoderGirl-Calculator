b994bd2a8757f5cbcdaa4d19f046b083297500da
LotusMel <46916651+LotusMel@users.noreply.github.com>
2019-02-15 23:26:45 -0600

    1st successful run

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -6,14 +6,58 @@ namespace Calculator
     {
         public static void Main()
         {
-            int result = 0;
+
+
+
+            Console.WriteLine("Please enter your first number: ");
+            float firstNumber = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter your second number: ");
+            float secondNumber = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("What would you like to do?: +, -, *, or /");
+            string math = (Console.ReadLine());
+
+            float result = 0;
+
+            if (math == "+")
+            {
+                result = firstNumber + secondNumber;
+            }
+            if (math == "-")
+            {
+                result = firstNumber - secondNumber;
+            }
+            if (math == "*")
+            {
+                result = firstNumber * secondNumber;
+            }
+            if (math == "/")
+            {
+                result = firstNumber / secondNumber;
+            }
+
+            Console.WriteLine($"The answer for your calculation is {result}");
+            Console.ReadLine();
+            
+        
+
+            
+            //Console.WriteLine($"Total = {result}");
+
+
+            
+            //char[] operators = new char[4];
+            
+
+            //converting string to number
+
 
             // Create a simple calculator that asks the user to input two different numbers 
             // and whether they want to add, subtract, multiply or divide them. 
 
-            Console.WriteLine($"Total = {result}");
 
-            Console.ReadLine();
+            //Console.ReadLine();
         }
     }
 }
